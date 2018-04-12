5776dba4ae228475d076a909697d3f13cb0edd3f
franny1615 <31251485+franny1615@users.noreply.github.com>
2018-04-11 17:48:43 -0700

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -48,18 +48,9 @@ def login():
  
 @app.route('/login/authorized')
 @google.authorized_handler
-def authorized():
-    print('In authorized()')
-    resp = google.authorized_response()
-    print('Printing response')
-    print(resp)
+def authorized(resp):
     if resp is None:
-        print('Access denied.')
-        return 'Access denied: reason=%s error=%s' % (
-            request.args['error_reason'],
-            request.args['error_description']
-        )
-    print('Access granted!')
+        me = 'Access denied: reason=%s error=%s' + request.args['error_reason'] + request.args['error_description']
     session['google_token'] = (resp['access_token'], '')
     me = google.get('userinfo')
     return render_template('home.html',info=me)
