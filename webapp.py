11d4ac7d00bab8c18b6a1d822db646f7c0e4d67d
franny1615 <31251485+franny1615@users.noreply.github.com>
2018-04-19 07:43:24 -0700

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -72,7 +72,7 @@ def showListings():
     table=""
     for doc in collection.find():
         if session['user_id'] in doc:
-            print("Your Listings per say: " + doc[session['user_id']])
+            print("Your Listings per say: " + str(doc[session['user_id']]))
         else:
             print(doc)
     table=Markup('<table><tr><td>Title</td><td>Paypal</td></tr><tr><td>'+ str(collection.find({session['user_id']:True})) +'</td><td>'+ str(collection.find({str(session['user_id']):"Listing"})) +'</td></tr></table>')
