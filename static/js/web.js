5fd57375b0b77d67aba0f1f1500d2e5ec9af779b
franny1615 <31251485+franny1615@users.noreply.github.com>
2018-04-19 09:41:21 -0700

    Update web.js

--- static/js/web.js
+++ static/js/web.js
@@ -1,5 +1,7 @@
 $(document).ready(function(){
 	$("#bois").click(function(){
-		$("#dog").load("/delete");
+		$("#dog").load("/delete", function(){
+		     $("#h2t").replaceWith("<h2>==>This text is updated</h2>");
+	        });
 	})
 });
