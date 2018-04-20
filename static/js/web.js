4a111269ee7e3ab59a7ce69b8b386a5cc46d74a8
franny1615 <31251485+franny1615@users.noreply.github.com>
2018-04-20 09:33:06 -0700

    Update web.js

--- static/js/web.js
+++ static/js/web.js
@@ -6,3 +6,18 @@ $(document).ready(function(){
 		$("#createListForm").load("/createListing",{'ltitle':$("#ltitle").val(),'ppemail':$("#paypaladdress").val(), 'description': $("#des").val()});
 	})
 });
+
+$(document).ready(function() {
+            namespace = '/'
+            var socket = io.connect(location.protocol + '//' + document.domain + ':' + location.port + namespace);
+			
+			//Event handler for the start event
+			socket.on('start', function(msg){
+				$('#start').text(msg); //display the server data
+			});
+			
+			//Event handler for my_response events
+			socket.on('my_response', function(msg){
+				$('#log').append('<br>' + msg);
+			});
+});
