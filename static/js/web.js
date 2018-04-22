3ab618e2e06a8140db26dfa6eee21295bdb1f0d4
franny1615 <31251485+franny1615@users.noreply.github.com>
2018-04-22 06:36:03 -0700

    Update web.js

--- static/js/web.js
+++ static/js/web.js
@@ -3,6 +3,8 @@ $(document).ready(function(){
 		$("#listingT").load("/deleteListing", {'id':$(".deleteButton").val()});
 	})
 	$(".createList").click(function(){
-		$("#createListForm").load("/createListing",{'ltitle':$("#ltitle").val(),'ppemail':$("#paypaladdress").val(), 'description': $("#des").val()});
+		$("#createListForm").load("/createListing",{'ltitle':$("#ltitle").val(),'ppemail':$("#paypaladdress").val(), 'description': $("#des").val()},function(){
+			$("#myModal").show();
+		});
 	})
 });
