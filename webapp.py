c73599e18b56411d778d10c749c21ad32a53cf9b
franny1615 <31251485+franny1615@users.noreply.github.com>
2018-04-22 07:29:26 -0700

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -84,7 +84,7 @@ def logout():
 @app.route('/createListing',methods=['POST'])
 def create_listing():
     collection.insert_one({session['user_id']:{"Listing":{"title":request.form['ltitle'],'description':request.form['des'],'paypaladdress':request.form['ppemail']}}})
-    return redirect(url_for('index'))
+    return showListings()
   
 @app.route('/deleteListing',methods=['POST'])
 def delete():
