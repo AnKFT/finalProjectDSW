11672a5f22f9dd49d1a2f956e2e011c8faaed2ea
lukeborders <luke.b0rd3rs@gmail.com>
2018-04-30 09:46:06 -0700

    ily <3

--- webapp.py
+++ webapp.py
@@ -75,7 +75,7 @@ def logout():
 @app.route('/uploadimg',methods=['GET','POST'])
 def upload_img():
     if request.method == 'POST':
-	    string = fs.put(request.files['file'], filename=request.files['file'].filename,Listing={"title":request.form['ltitle'], 'description':request.form['des'],'paypaladdress':request.form['ppemail'],'user_id':session['user_id']})
+	    string = fs.put(request.files['file'], filename=request.files['file'].filename,Listing={"title":request.form['ltitle'],'price':request.form['pprice'], 'description':request.form['des'],'paypaladdress':request.form['ppemail'],'user_id':session['user_id']})
     return redirect(url_for('index'))
 
 @app.route('/createListing',methods=['POST'])
@@ -100,6 +100,8 @@ def showListings():
             tablestr += str(doc['Listing']['description'])
             tablestr += "</td><td>"
             tablestr += str(doc['Listing']['paypaladdress'])
+            tablestr += "</td><td>"
+            tablestr += str(doc['Listing']['price'])
             tablestr += "</td><td>"
             tablestr += '<button class="btn btn-danger deleteBtn" onclick="deletefunction()" value="' + str(doc.get('_id')) + '">Delete</button></td></tr>'
     tablestr += "</table>"
