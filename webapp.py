9a21fc01bb700b7833b734b1938608286cefd860
programming-john <31251360+programming-john@users.noreply.github.com>
2018-05-21 09:23:21 -0700

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -75,16 +75,13 @@ def logout():
   
 @app.route('/uploadimg',methods=['GET','POST'])
 def upload_img():
-	try:
-		if request.method == 'POST':
-			if 'file' not in request.files or request.form['ltitle'] == '' or request.form['pprice'] == '' or request.form['des'] == '' or request.form['ppemail'] == '':
-				flash("You did not fill in all the fields.")
-			else:
-				string = fs.put(request.data['file'], filename=request.files['file'].filename,Listing={"title":request.form['ltitle'],'price':request.form['pprice'],'category':request.form['thecategory'], 'description':request.form['des'],'paypaladdress':request.form['ppemail'],'user_id':session['user_id']})
-	except Exception as e:
-		print(e)
-		return redirect(url_for('index'))
-	return redirect(url_for('index'))
+if request.method == 'POST':
+	if 'file' not in request.files or request.form['ltitle'] == '' or request.form['pprice'] == '' or request.form['des'] == '' or request.form['ppemail'] == '':
+		flash("You did not fill in all the fields.")
+	else:
+		string = fs.put(request.data['file'], filename=request.files['file'].filename,Listing={"title":request.form['ltitle'],'price':request.form['pprice'],'category':request.form['thecategory'], 'description':request.form['des'],'paypaladdress':request.form['ppemail'],'user_id':session['user_id']})
+
+return redirect(url_for('index'))
   
 @app.route('/deleteListing',methods=['POST'])
 def delete():
